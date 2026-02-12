9092286dcf0b5f169fad27813343cad9f9566048
Loran <danilmockhnatkin@gmail.com>
2026-02-13 01:00:54 +0400

    Доработка коннекта с БД

--- logic/user.php
+++ logic/user.php
@@ -1,18 +1,69 @@
 <?php
-$servername = "localhost";
-$username = "cw809330";
-$password = "uzhBMnT7";
-$dbname   = "cw809330_test";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+function getDbConnection() {
+    $servername = "localhost";
+    $username = "cw809330";
+    $password = "uzhBMnT7";
+    $dbname   = "cw809330_test";
 
-if (!$conn) {
-    die("Соединение не удалось: " . mysqli_connect_error());
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$conn) {
+        die("Соединение не удалось: " . mysqli_connect_error());
+    }
+
+    return $conn;
 }
 
-echo "Соединение установлено успешно!";
+function getData($table, $fields = '*', $where = '') {
+    $conn = getDbConnection();
 
-// Здесь можно выполнять запросы к БД
+    $query = "SELECT $fields FROM $table";
+    if (!empty($where)) {
+        $query .= " WHERE $where";
+    }
 
-mysqli_close($conn);
+    $result = mysqli_query($conn, $query);
+    $data = [];
+
+    if ($result) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row;
+        }
+    }
+
+    mysqli_close($conn);
+    return $data;
+}
+
+function insertData($table, $data) {
+    $conn = getDbConnection();
+
+    $fields = implode(', ', array_keys($data));
+    $values = implode("', '", array_map('mysqli_real_escape_string', $conn, $data));
+
+    $query = "INSERT INTO user_data ($fields) VALUES ('$values')";
+
+    $success = mysqli_query($conn, $query);
+
+    mysqli_close($conn);
+    return $success;
+}
+
+function updateData($table, $data, $where) {
+    $conn = getDbConnection();
+
+    $updates = [];
+    foreach ($data as $field => $value) {
+        $updates[] = "$field = '" . mysqli_real_escape_string($conn, $value) . "'";
+    }
+    $updateStr = implode(', ', $updates);
+
+    $query = "UPDATE $table SET $updateStr WHERE $where";
+
+    $success = mysqli_query($conn, $query);
+
+    mysqli_close($conn);
+    return $success;
+}
 ?>
